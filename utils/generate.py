615711d7a667d93fb2ee6756402efcd95121f89c
wenshicheng97 <wenshicheng97@gmail.com>
2023-06-10 14:49:23 -0700

    Webtext generation

--- utils/generate.py
+++ utils/generate.py
@@ -6,11 +6,12 @@ import argparse
 import os
 import nltk
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaTokenizer, LlamaForCausalLM
 from utils.process import *
 
-DATASETS = ['roc', 'arxiv']
-MODELS = ['llama-7b', 'llama-13b', 'alpaca-7b', 'vicuna-7b', 'vicuna-13b']
+DATASETS = ['roc', 'arxiv', 'webtext']
+MODELS = ['llama', 'alpaca', 'vicuna']
+SIZES = ['7b', '13b']
 
 
 def set_seed(seed):
@@ -22,26 +23,34 @@ def set_seed(seed):
 def generate(**kwargs):
     current = time.time()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model_name = kwargs['model'] + '-' + kwargs['model_size']
 
-    path_to_model = kwargs['path_to_model'] if kwargs['path_to_model'] else os.path.join('weights', kwargs['model'])
+    path_to_model = kwargs['path_to_model'] if kwargs['path_to_model'] else os.path.join('weights', model_name)
 
     # Load model
-    tokenizer = AutoTokenizer.from_pretrained(path_to_model)
-    model = AutoModelForCausalLM.from_pretrained(path_to_model)
+    tokenizer = LlamaTokenizer.from_pretrained(path_to_model)
+    model = LlamaForCausalLM.from_pretrained(path_to_model, torch_dtype=torch.float16)
     model = model.to(device)
-
+    # os.system("nvidia-smi")
+    print("Load model time:", time.time() - current)
+    start_time = current
+    current = time.time()
+    taskid = kwargs['taskid']
     # Load data
-    fdata = open(os.path.join(kwargs['data_dir'], kwargs['data_name'], 'real.jsonl'), 'r')
+    input_file = os.path.join(kwargs['data_dir'], kwargs['data_name'], f'real{taskid}.jsonl') if kwargs['input_file'] is None else kwargs['input_file']
+    fdata = open(input_file, 'r') 
 
     match kwargs['data_name']:
         case 'roc':
             processor = RocProcessor(kwargs['model'], 'continue')
         case 'arxiv':
-            processor = ArxivProcessor(kwargs['model'], 'continue')
+            processor = ArxivProcessor(kwargs['model'], 'rewrite')
+        case 'webtext':
+            processor = WebtextProcessor(kwargs['model'], 'rewrite')
 
     for idx, line in enumerate(fdata):
         if kwargs['time_limit'] is not None:
-            if time.time() - current >= kwargs['time_limit'] * 60:
+            if time.time() - start_time >= kwargs['time_limit'] * 60:
                 break
 
         if kwargs['generation_count'] == 0:
@@ -49,17 +58,15 @@ def generate(**kwargs):
 
         sample = json.loads(line)
         if idx >= kwargs['start']:
-            print('Generating:', idx)
-            text = sample['text']
-
+            # print('Generating:', idx)
+            os.system(f"echo Generating: {idx}")
+            text = processor.process_text(processor.get_text(sample))
             text_length = len(tokenizer.encode(text))
-         
-            inputs = processor.get_inputs(tokenizer, text).to(device)
-
-
+            input_text = processor.get_inputs(processor.process_text(text))
+            inputs = tokenizer(input_text, return_tensors='pt').to(device)
             generate_ids = model.generate(inputs.input_ids,
-                                          min_length=text_length - kwargs['window'],
-                                          max_length=text_length + kwargs['window'],
+                                          min_new_tokens=text_length - kwargs['window'],
+                                          max_new_tokens=text_length + kwargs['window'],
                                           do_sample=kwargs['do_sample'],
                                           num_beams=kwargs['num_beams'],
                                           temperature=kwargs['temperature'],
@@ -71,25 +78,32 @@ def generate(**kwargs):
                                             skip_special_tokens=True,
                                             clean_up_tokenization_spaces=False)[0]
 
-            json_result = {'id': idx, 'text': result}
-
-            output_file = os.path.join(kwargs['data_dir'], 'test', kwargs['data_name'], 'fake.jsonl')
+            json_result = {'id': idx, 'text': result[len(input_text):], 'model': model_name}
+            output_dir = os.path.join(kwargs['data_dir'], 'test', kwargs['data_name'])
+            if not os.path.isdir(output_dir):
+                os.mkdir(output_dir)
+            output_file = os.path.join(output_dir, f'fake{taskid}.jsonl') if kwargs['output_file'] is None else kwargs['output_file']
             with open(output_file, 'a') as f:
                 f.write(json.dumps(json_result) + "\n")
             kwargs['generation_count'] -= 1
+    print("Generation time:", time.time() - current)
+    print("Total time:", time.time() - start_time)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--model", type=str, default="llama-7b", choices=MODELS)
+    parser.add_argument("--model", type=str, default="llama", choices=MODELS)
+    parser.add_argument("--model-size", type=str, default='7b', choices=SIZES)
     parser.add_argument("--path-to-model", type=str, default=None)
     parser.add_argument("--data-name", type=str, default='roc', choices=DATASETS)
     parser.add_argument("--data-dir", type=str, default='data')
+    parser.add_argument("--input-file", type=str, default=None)
     parser.add_argument("--generation-count", type=int, default=5)
     parser.add_argument("--time-limit", type=int, default=None, help='in miniutes')
     parser.add_argument("--start", type=int, default=0, help='starting index of the data')
-    parser.add_argument("--output-dir", type=str, default='data/fakes')
+    parser.add_argument("--output-dir", type=str, default=None)
+    parser.add_argument("--output-file", type=str, default=None)
     parser.add_argument("--window", type=int, default=5, help='flucuation window of the orignal length')
     parser.add_argument("--do-sample", type=bool, default=True)
     parser.add_argument("--num-beams", type=int, default=1)
@@ -97,6 +111,7 @@ def get_args():
     parser.add_argument("--top-k", type=int, default=50)
     parser.add_argument("--top-p", type=float, default=1.0)
     parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+    parser.add_argument("--taskid", type=str, default='')
   
     args = parser.parse_args()
 
